5298d68d9ace74cfdfb251afd54145245619f9e9
Nathalie Goffette <nathaliegoffette@gmail.com>
2023-03-06 10:07:03 +0100

    input

---
+++ src/Input.js
@@ -0,0 +1,13 @@
+
+function Input() {
+    return (
+      <div className="input">
+            <label for="newtodo">New "to do":</label><br/>
+            <input type="text" id="newtodo" name="newtodo" placeholder='type a new "to do"'>
+
+      </div>
+    );
+  }
+  
+  export default Input;
+  
